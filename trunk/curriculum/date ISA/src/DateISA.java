b2f35ec5f1c35dff5f5731fe4f62020793546a5e
Nate Titterton <nate@ubuntu.(none)>
2012-06-19 16:31:41 -0700

    moving DateISATest into jar, adding launch

--- trunk/curriculum/date ISA/src/DateISA.java
+++ trunk/curriculum/date ISA/src/DateISA.java
@@ -14,7 +14,7 @@ public class DateISA {
 	public boolean isOK ( ) {
 		int leapDay;
 		if (myYear % 4 != 0) {
-			leapDay = 0;
+			leapDay = 1;
 		} else if (myYear % 100 != 0) {
 			leapDay = 1;
 		} else if (myYear % 400 != 0) {
