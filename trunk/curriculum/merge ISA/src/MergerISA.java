387a4f5079332830a40020e03e5b87d0557da958
Michael Clancy <clancy@cs.berkeley.edu>
2012-06-20 13:19:15 -0700

    Fixing up debugging exercises

--- trunk/curriculum/merge ISA/src/MergerISA.java
+++ trunk/curriculum/merge ISA/src/MergerISA.java
@@ -1,13 +1,5 @@
 
 public class MergerISA {
-	
-	private static String toString (int [ ] a) {
-		String rtn = "";
-		for (int k=0; k<a.length; k++) {
-			rtn = rtn + a[k] + " ";
-		}
-		return rtn;
-	}
 
 	// Combine arrays a and b, sorted in increasing order, 
 	// into an new array rtn that's also sorted in increasing order.
@@ -16,20 +8,20 @@ public class MergerISA {
 		// aIndex and bIndex keep track of values in a and b already added to rtn.
 		// Invariant: aIndex + bIndex == rIndex, that is, aIndex values from a
 		// and bIndex values from b have been added to rtn.
-		int aIndex = 0;
-		int bIndex = 0;
-		int rIndex = 0;
-		while (aIndex < a.length && bIndex < b.length) {
-			if (a[aIndex] < b[bIndex]) {
+		int aIndex = 0;											// #1
+		int bIndex = 0;											// #2
+		int rIndex = 0;											// #3
+		while (aIndex < a.length && bIndex < b.length) {		// #4
+			if (a[aIndex] < b[bIndex]) {						// #5
 				// The next element in a is to be added to rtn.
-				rtn[rIndex] = a[aIndex];
-				aIndex++;
-				rIndex++;
+				rtn[rIndex] = a[aIndex];						// #6
+				aIndex++;										// #7
+				rIndex++;										// #8
 			} else {
 				// The next element in b is to be added to rtn.
-				rtn[rIndex] = b[aIndex];
-				bIndex++;
-				rIndex++;
+				rtn[rIndex] = a[bIndex];						// #9
+				bIndex++;										// #10
+				rIndex++;										// #11
 			}
 			if (!isOK (a, b, rtn, aIndex, bIndex, rIndex)) {
 				System.out.println ("inconsistency 1: aIndex = " + aIndex
@@ -40,10 +32,10 @@ public class MergerISA {
 		}
 		// One of arrays a and b has run out of values.
 		// Add the remaining values to rtn.
-		while (aIndex < a.length) {
-			rtn[rIndex] = a[aIndex];
-			aIndex++;
-			rIndex++;
+		while (aIndex < a.length) {								// #12
+			rtn[rIndex] = a[aIndex];							// #13
+			aIndex++;											// #14
+			rIndex++;											// #15
 			if (!isOK (a, b, rtn, aIndex, bIndex, rIndex)) {
 				System.out.println ("inconsistency 2: aIndex = " + aIndex
 					+ "; bIndex = " + bIndex + "; rIndex = " + rIndex);
@@ -51,10 +43,10 @@ public class MergerISA {
 						+ toString(b) + "; rtn = " + toString (rtn));
 			}
 		}
-		while (bIndex < b.length) {
-			rtn[rIndex] = b[bIndex];
-			bIndex++;
-			rIndex++;
+		while (bIndex < b.length) {								// #16
+			rtn[rIndex] = b[bIndex];							// #17
+			bIndex++;											// #18
+			rIndex++;											// #19
 			if (!isOK (a, b, rtn, aIndex, bIndex, rIndex)) {
 				System.out.println ("inconsistency 3: aIndex = " + aIndex
 					+ "; bIndex = " + bIndex + "; rIndex = " + rIndex);
@@ -81,6 +73,40 @@ public class MergerISA {
 				return false;
 			}
 		}
+		for (int k=0; k<rIndex; k++) {
+			if (!contains (a, r[k]) && !contains (b, r[k])) {
+				return false;
+			}
+		}
+		for (int k=0; k<aIndex; k++) {
+			if (!contains (r, a[k])) {
+				return false;
+			}
+		}
+		for (int k=0; k<bIndex; k++) {
+			if (!contains (r, b[k])) {
+				return false;
+			}
+		}
 		return true;
+	}
+
+	// Return a printable form of the given array.
+	private static String toString (int [ ] values) {
+		String rtn = "{ ";
+		for (int k=0; k<values.length; k++) {
+			rtn = rtn + values[k] + " ";
+		}
+		return rtn + "}";
+	}
+	
+	// Return true if the given array contains the given value.
+	private static boolean contains (int [ ] values, int x) {
+		for (int k=0; k<values.length; k++) {
+			if (values[k] == x) {
+				return true;
+			}
+		}
+		return false;
 	}
 }
--- trunk/curriculum/merge ISA/src/MergerISATest.java
+++ trunk/curriculum/merge ISA/src/MergerISATest.java
@@ -1,16 +1,22 @@
 import junit.framework.TestCase;
-import java.util.*;
-
-import org.junit.runner.JUnitCore;
+import java.util.Arrays;
 
 public class MergerISATest extends TestCase {
+	
+	private static String toString (int [ ] values) {
+		String rtn = "{ ";
+		for (int k=0; k<values.length; k++) {
+			rtn = rtn + values[k] + " ";
+		}
+		return rtn + "}";
+	}
 	
 	public void testOneElemMerge ( ) {
 		int [ ] a = {1};
 		int [ ] b = {2};
 		int [ ] desired = {1,2};
-		assertTrue (Arrays.equals (MergerISA.merged (a, b), desired));
-		assertTrue (Arrays.equals (MergerISA.merged (b, a), desired));
+		assertTrue ("fails when merging {1} and {2}", Arrays.equals (MergerISA.merged (a, b), desired));
+		assertTrue ("fails when merging {2} and {1}", Arrays.equals (MergerISA.merged (b, a), desired));
 	}
 	
 	public void testTwoElemsMerge ( ) {
@@ -18,7 +24,8 @@ public class MergerISATest extends TestCase {
 		int [ ] [ ]	bTests = {{3,4}, {2,4}, {2,3}, {1,4}, {1,3}, {1,2}};
 		int [ ] desired = {1,2,3,4};
 		for (int k=0; k<aTests.length; k++) {
-			assertTrue (Arrays.equals (MergerISA.merged (aTests[k], bTests[k]), desired));
+			assertTrue ("fails when merging " + toString (aTests[k]) + " and " + toString (bTests[k]),
+					Arrays.equals (MergerISA.merged (aTests[k], bTests[k]), desired));
 		}
 	}
 }
