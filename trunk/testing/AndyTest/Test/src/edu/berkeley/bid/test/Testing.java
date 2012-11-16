ebb699c044f0b700a393213973f12052e0cc5dc9
Nathaniel Titterton <nate@berkeley.edu>
2012-11-16 11:12:17 -0800

    random stuff to Andy test

--- trunk/testing/AndyTest/Test/src/edu/berkeley/bid/test/Testing.java
+++ trunk/testing/AndyTest/Test/src/edu/berkeley/bid/test/Testing.java
@@ -2,4 +2,21 @@ package edu.berkeley.bid.test;
 
 public class Testing {
 
+	
+	public static void tryRecursion (int i ) {
+		System.out.println ("I'm here " + i);
+		tryRecursion (i + 1 );
+	}
+
+	
+	public static String tryRecursion2() {
+		return "again!" + tryRecursion2();
+	}
+	
+	
+	
+	public static void main (String [ ] args) {
+		tryRecursion (1 );
+	}
+
 }
