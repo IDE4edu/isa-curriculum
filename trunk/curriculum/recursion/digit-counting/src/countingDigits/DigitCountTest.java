1c7d0060a5f16d17d3912a5af74c264a3331f4c9
Nathaniel Titterton <nate@berkeley.edu>
2012-12-10 00:48:39 -0800

    fixed expected v. actual in tests

--- trunk/curriculum/recursion/digit-counting/src/countingDigits/DigitCountTest.java
+++ trunk/curriculum/recursion/digit-counting/src/countingDigits/DigitCountTest.java
@@ -8,28 +8,28 @@ public class DigitCountTest {
 
 	@Test
 	public void digit_count_of_314159() {
-		assertEquals("Incorrect, with a call of digitCount(314159)", DigitCount.digitCount(314159), 6);
+		assertEquals("Incorrect, with a call of digitCount(314159)", 6, DigitCount.digitCount(314159));
 	}
 
 	
 	@Test
 	public void digit_count_of_0() {
-		assertEquals("Incorrect, with a call of digitCount(0)", DigitCount.digitCount(0), 1);
+		assertEquals("Incorrect, with a call of digitCount(0)", 1, DigitCount.digitCount(0));
 	}
 	
 	@Test
 	public void digit_count_of_8() {
-		assertEquals("Incorrect, with a call of digitCount(8)", DigitCount.digitCount(8), 1);
+		assertEquals("Incorrect, with a call of digitCount(8)", 1, DigitCount.digitCount(8));
 	}
 	
 	@Test
 	public void digit_count_of_1040() {
-		assertEquals("Incorrect, with a call of digitCount(1040)", DigitCount.digitCount(1040), 4);
+		assertEquals("Incorrect, with a call of digitCount(1040)", 4, DigitCount.digitCount(1040));
 	}
 	
 	@Test
 	public void digit_count_of_314159265() {
-		assertEquals("Incorrect, with a call of digitCount(314159265)", DigitCount.digitCount(314159265), 9);
+		assertEquals("Incorrect, with a call of digitCount(314159265)", 9, DigitCount.digitCount(314159265));
 	}
 	
 }
