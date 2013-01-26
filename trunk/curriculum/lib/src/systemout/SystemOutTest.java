6721f585cbb1de2582d9a11bc1937d57bf2d6d44
Nathaniel Titterton <nate@berkeley.edu>
2013-01-25 22:56:20 -0800

    added ability to specify input, and went back to a callback to run users code that gets overridden.

--- trunk/curriculum/lib/src/systemout/SystemOutTest.java
+++ trunk/curriculum/lib/src/systemout/SystemOutTest.java
@@ -1,63 +1,72 @@
 package systemout;
-
-
 import static org.junit.Assert.*;
+
+import java.io.ByteArrayInputStream;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-import org.junit.*;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 
 
 public class SystemOutTest {
 
 	
-	// Extend this class in your test class
+	// a- Extend this class in your test class
+	// b- override callUserCode() with whatever you need to do to have 
+	//    the user generate the System.out.println calls
 	//
 	// For each test/set-of-tests for a particular run of user code
-	//  1- run the user's code that prints to System.out
-	//  2- call collectUserOutput();
-	//  3- call the relevant assertSO... methods
+	//  1- call collectUserOutput() or collectUserOuput(String[] userInput).
+	//     This will invoke callUserCode() for you.
+	//  2- call the relevant assertSO... methods
  
+	// if the users code will require inputs, you should make a String[]
+	// or List<String>, with one String for each input.  You can set the input
+	// to use for a particular run of the users code with either a 
+	// call to collectUserOuput(String[]) (or with List<String>),
+	// or by invoking setUserInput(String[]) (or with List<String>) followed
+	// by collectUserOutput()
 	
 	
-	// will hold the lines from the last set of user output
-	public static List<String> userOutput = new ArrayList<String>();
+	/////////
+	
 
 	
 	
 	////// SysOut asserts
 
-	public static void assertSOContainsLine(String message, String line) {
+	public void assertSOContainsLine(String message, String line) {
 		assertTrue(message, userOutput.contains(line));
 	}
 	
-	public static void assertSOHasLine(String message, int index, String line) {
+	public void assertSOHasLine(String message, int index, String line) {
 		assertTrue(message, userOutput.get(index).equals(line));
 	}
 	
 	
-	public static void assertSOContainsLines(String message, String[] lines) {
+	public void assertSOContainsLines(String message, String[] lines) {
 		fail("unimplemented");
 	}
 	
-	public static void assertSOContainsLines (String message, List<String> lines) {
+	public void assertSOContainsLines (String message, List<String> lines) {
 		fail("unimplemented");
 	}
 	
-	public static void assertSOEquals (String message, String[] lines) {
+	public void assertSOEquals (String message, String[] lines) {
 		assertArrayEquals(message, userOutput.toArray(), lines); 
 	}
 	
-	public static void assertSOEquals (String message, List<String> lines) {
+	public void assertSOEquals (String message, List<String> lines) {
 		assertArrayEquals(message, userOutput.toArray(), lines.toArray());
 	}
 	
-	public static void assertSONumLines(String message, int numLines) {
+	public void assertSONumLines(String message, int numLines) {
 		assertTrue(message, userOutput.size() == numLines);
 	}
 	
@@ -66,52 +75,93 @@ public class SystemOutTest {
 	
 	
 	/////////////
+
 	
-	// call this after running the user's code
-	public static void collectUserOutput() {
-		// user code just got run, hopefully
+	// will hold the lines from the last set of user output
+	public List<String> userOutput = new ArrayList<String>();
 
-		userOutput.clear();
-		while (userOutputScanner.hasNextLine()) {
-			userOutput.add(userOutputScanner.nextLine());
-		}
-
-		preUserRunSetUp();
+	
+	// override this
+	public void callUserCode() {
+		//throw new Exception("Method callUserCode should be overriden, and super should not be called.");
+		// urk, keeping this in makes ugly throw declarations necessary
 	}
 	
-	public static String filenamePrefix = "tmpSysoutTest";
-	static Scanner userOutputScanner;
+	
+	public void setUserInput(String[] input) {
+		StringBuffer tempSB = new StringBuffer();
+		String separator = System.getProperty("line.separator");
+		for (int i = 0; i < input.length; i++) {
+		   tempSB.append( input[i] );
+		   tempSB.append( separator );
+		}
+		InputStream is = new ByteArrayInputStream(tempSB.toString().getBytes());
+		System.setIn(is);
+	}
+	
+	public void setUserInput(List<String> input) {
+		String[] temp;
+		temp = new String[input.size()];
+		setUserInput(input.toArray(temp));
+	}
+	
+	
+	public void collectUserOutput(String[] input) {
+		setUserInput(input);
+		collectUserOutput();
+	}
+	
+	public void collectUserOutput(List<String> input) {
+		setUserInput(input);
+		collectUserOutput();
+	}
+	
 
-	private static void preUserRunSetUp() {
+	public static String filenamePrefix = "tmpSysoutTest";
+	Scanner userOutputScanner;
+	
+	// call this to run the users code
+	public void collectUserOutput() {
 		File userOutputTempFile;
 		try {
 			userOutputTempFile = File.createTempFile(filenamePrefix, null);
 			PrintStream userOutputTempStream = new PrintStream (userOutputTempFile);
 			System.setOut (userOutputTempStream);
 			userOutputScanner = new Scanner(userOutputTempFile);
-			oldPrintStreams.add(userOutputTempStream);
-			oldFiles.add(userOutputTempFile);
+			
+			callUserCode();
+			
+			userOutput.clear();
+			while (userOutputScanner.hasNextLine()) {
+				userOutput.add(userOutputScanner.nextLine());
+			}
+			
+			userOutputTempStream.close();
+			userOutputTempFile.delete();
+			
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-	}
 
+		
+	}
+	
+	
+	
+	private static PrintStream originalOut;
+	private static InputStream originalIn;
 	
 	@BeforeClass
-	public static void initialSetup() {
-		preUserRunSetUp();
+	public static void setup() {
+		originalOut = System.out;
+		originalIn = System.in;
 	}
-	
-	static List<PrintStream> oldPrintStreams = new ArrayList<PrintStream>();
-	static List<File> oldFiles = new ArrayList<File>();
-	
+
 	@AfterClass
-	public static void tearDown() {
-		for (PrintStream ps : oldPrintStreams) {
-			ps.close();
-		}
-		for (File f : oldFiles) {
-			f.delete();
-		}
+	public static void teardown() {
+		System.setOut(originalOut);
+		System.setIn(originalIn);
+		
 	}
+
 }
--- trunk/curriculum/lib/src/tests/UserResponsesTest2.java
+++ trunk/curriculum/lib/src/tests/UserResponsesTest2.java
@@ -13,11 +13,15 @@ public class UserResponsesTest2 extends SystemOutTest {
 
 	
 
-	@Before
-	public void doit() {
+	@Override
+	public void callUserCode() {
 		String[] empty = {""};
 		UserCode.main(empty);
-		
+	}
+	
+	@Before
+	public void doit() {
+		// we don't need to set up different input
 		collectUserOutput();
 	}
 	
