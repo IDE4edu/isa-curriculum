471d366f16eff39449f2ac15ba3949c5d0f40a62
Nathaniel Titterton <nate@berkeley.edu>
2013-01-28 11:07:21 -0800

    better testing of input stuff in sysouttest.  Still some weirdnesses though

---
+++ trunk/curriculum/lib/src/tests/SysoutWithInput.java
@@ -0,0 +1,19 @@
+package tests;
+
+import java.util.Scanner;
+
+public class SysoutWithInput {
+	
+	public static int num = 5;
+	static Scanner input = new Scanner(System.in);
+
+	public static void main (String [ ] args) {
+		System.out.println ("orig = " + num);
+		num++;
+		System.out.println ("new = " + num);
+		System.out.println("gimme an int: ");
+		int u = input.nextInt();
+		System.out.println("Adding 1 to your number gives us " + (u+1));
+//		System.out.println ("foo");
+	}
+}
---
+++ trunk/curriculum/lib/src/tests/SysoutWithInputTest.java
@@ -0,0 +1,52 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import systemout.SystemOutTest;
+
+public class SysoutWithInputTest extends SystemOutTest {
+
+	@Override
+	public void callUserCode() {
+		String[] empty = {""};
+		SysoutWithInput.main(empty);
+	}
+	
+	@Test
+	@Ignore
+	public void test1() {
+		String[] in = {"45"};
+		collectUserOutput(in);
+		assertSOContainsLine("You didn't handle 45 as input correctly",
+				"Adding 1 to your number gives us 46");
+		System.out.println(userOutput);
+	}
+
+	
+	@Test
+	public void test2() {
+		List<String> in = new ArrayList<String>();
+		in.add("299");
+		in.add("300");
+		setUserInput(in);
+		collectUserOutput();
+		assertSOContainsLine("You didn't handle 45 as input correctly",
+				"Adding 1 to your number gives us 301");
+	}
+	
+	@Test
+	@Ignore
+	public void test3() {
+		List<String> in = new ArrayList<String>();
+		in.add("0");
+		collectUserOutput(in);
+		assertSOContainsLine("Doesn't have the first line yo", "orig = 5");
+		assertSOContainsLine("yo", "Adding 1 to your number gives us 1");
+	}
+}
