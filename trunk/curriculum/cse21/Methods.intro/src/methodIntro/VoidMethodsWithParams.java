2d0be9d3333482f34c96ad109448635efa152f6f
Nathaniel Titterton <nate@berkeley.edu>
2013-02-11 15:45:59 -0800

    cleaning up syntax in instructions

--- trunk/curriculum/cse21/Methods.intro/src/methodIntro/VoidMethodsWithParams.java
+++ trunk/curriculum/cse21/Methods.intro/src/methodIntro/VoidMethodsWithParams.java
@@ -1,20 +1,20 @@
 package methodIntro;
 
 // Exercise 1: 
-// Add a loop to m1  so that it the number of "A"s printed
+// Add a loop to m1 so that the number of "A"s printed
 // is equal to lineCount.
 
 // Exercise 2:
 // Modify m2 so that the total number of lines printed is equal to lineCount, with
-// half the lines printed contain a "A" and the other half contain an "B". 
-// If lineCount is odd, the number of B’s printed should be 1 less than the number of A’s.
+// half the lines printed containing an "A" and the other half containing a "B". 
+// If lineCount is odd, the number of "B"s printed should be 1 less than the number of "A"s.
 // For example, 
 //  	m2 (4)
-// should print 2 B’s and 2 A’s, and
+// should print 2 "B"s and 2 "A"s, and
 //  	m2 (7)
-// should print 3 B’s and 4 A’s.  The order of the "B"s and "A"s aren't important.
+// should print 3 "B"s and 4 "A"s.  The order of the "B"s and "A"s aren't important.
 
-// Test your solutions by clicking the 'test' button, or by manually changing main
+// Test your solutions by clicking the 'test' button, or by manually changing main.
 
 public class VoidMethodsWithParams {
 
