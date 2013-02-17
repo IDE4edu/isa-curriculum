d24ca3c4cfe684142410e34a555267c171de6f19
Nathaniel Titterton <nate@berkeley.edu>
2013-02-17 07:00:37 -0800

    initial commit cse21 lab5 -- but, nothing done really.

---
+++ trunk/curriculum/cse21/lab5/src/TEST_for_GenCheeseShopv2.java
@@ -0,0 +1,15 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import edu.berkeley.eduride.tests.*;
+
+
+public class TEST_for_GenCheeseShopv2 extends EduRideTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
