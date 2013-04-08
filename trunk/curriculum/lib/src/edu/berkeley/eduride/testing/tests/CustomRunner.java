b634dc8597352c564c120eda8bd7abb98f4810ec
Nathaniel Titterton <nate@berkeley.edu>
2013-04-08 16:11:56 -0700

    trying desperately to get information across with custom runner...  and failing...

--- trunk/curriculum/lib/src/edu/berkeley/eduride/testing/tests/CustomRunner.java
+++ trunk/curriculum/lib/src/edu/berkeley/eduride/testing/tests/CustomRunner.java
@@ -3,55 +3,37 @@ package edu.berkeley.eduride.testing.tests;
 import java.lang.reflect.Method;
 import java.util.List;
 
+import junit.framework.AssertionFailedError;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Ignore;
+import org.junit.internal.AssumptionViolatedException;
+import org.junit.internal.runners.model.EachTestNotifier;
 import org.junit.runner.Description;
+import org.junit.runner.notification.RunNotifier;
 import org.junit.runners.BlockJUnit4ClassRunner;
 import org.junit.runners.model.FrameworkMethod;
 import org.junit.runners.model.InitializationError;
+import org.junit.runners.model.Statement;
 
-public class CustomRunner extends BlockJUnit4ClassRunner{
+public class CustomRunner extends BlockJUnit4ClassRunner {
 
 	int counter = 0;
 
 	public CustomRunner(Class<?> klass) throws InitializationError {
 		super(klass);
-
-		// TODO Goal: Get squareTest to run and add another test case that 
-		// has the @Ignored annotation
-
-	}
-	//
-	//	public TheRunner(Class<? extends MyTargetTestClass> testClass) { 
-	//		this.testClass = testClass;
-	//		testContainingInstance = reflectMeATestContainingInstance(testClass);
-	//		testSuiteDescription = Description.createSuiteDescription("All my stuff is happening now dudes");
-	//		testSuiteDescription.addChild(createTestDescription("first bit happening"));
-	//		testSuiteDescription.addChild(createTestDescription("second bit happening"));
-	//		testSuiteDescription.addChild(createTestDescription("third bit happening"));
-	//	}
-
-	@Deprecated
-	protected void validateInstanceMethods(List<Throwable> errors) {
-		validatePublicVoidNoArgMethods(After.class, false, errors);
-		validatePublicVoidNoArgMethods(Before.class, false, errors);
-		validateTestMethods(errors);
-
-		if (super.computeTestMethods().size() == 0)
-			errors.add(new Exception("No runnable methods"));
 	}
 
-	@Ignore
-	public void myFakeTest(){
-	}
+
+
 
 	@Override
-	public List<FrameworkMethod> computeTestMethods(){
-		List<FrameworkMethod> frameworkMethods = super.computeTestMethods();
-		Method fakeMethod = null;
+	public List<FrameworkMethod> computeTestMethods() {
+		List<FrameworkMethod> fms = super.computeTestMethods();
+		Method ignoredTestMethod = null;
 		try {
-			fakeMethod = CustomRunner.class.getMethod("myFakeTest", null);
+			ignoredTestMethod = CustomRunner.class.getMethod("ignoredMethod", null);
 		} catch (SecurityException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -59,10 +41,60 @@ public class CustomRunner extends BlockJUnit4ClassRunner{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		if (!frameworkMethods.contains(fakeMethod)){
-			frameworkMethods.add(new FrameworkMethod(fakeMethod));
+
+		boolean seenIt = false;
+		for (FrameworkMethod fm : fms) {
+			if (fm.getClass().equals(RunEvenIfIgnored.class)) {
+				seenIt = true;
+			}
 		}
-		return frameworkMethods;
+		if (!seenIt) {
+			fms.add(new RunEvenIfIgnored(ignoredTestMethod));
+		}
+		return fms;
 	}
+
+
+
+	protected void runChild(final FrameworkMethod method, RunNotifier notifier) {
+		Description description = describeChild(method);
+		if (method.getClass().equals(RunEvenIfIgnored.class)) {
+			EachTestNotifier eachNotifier = new EachTestNotifier(notifier, description);
+			//eachNotifier.addFailedAssumption(new AssumptionViolatedException("CAN YOU SEE ME ASSUMPTION?"));
+	        //eachNotifier.addFailure(new AssertionFailedError("CAN YOU SEE ME?"));
+			eachNotifier.addFailure(new EduRideCommunication("CAN YOU SEE ME EDURIDE?"));
+	        notifier.fireTestIgnored(description);
+		}
+		else {
+			super.runChild(method, notifier);
+		}
+	}
+
+	
+	public class EduRideCommunication extends Throwable {
+		private static final long serialVersionUID = 3726556059859848779L;
+
+		public EduRideCommunication(String msg) {
+			super(msg);
+		}
+	}
+	
+	
+	public class RunEvenIfIgnored extends FrameworkMethod {
+
+		public RunEvenIfIgnored(Method m) {
+			super(m);
+		}
+		
+		@Override
+		public String getName() {
+			return "Please Ignore This J@#K#@J@J@K#J%JJFDJsdkkejwo87fkshDw28eswh@#fjwofhwehowioi92039u5u029efowijf029je0f9j09jd09gfuw0d9fu0s9dfu0s9djf0sw9jf09sudf0wohjtru3g98yu43989few8rfijdsfijs09d8fjw08ej98rfj298fj8je8wjf08wjef08udsoigtjlkjfwo9vrewf98wu0t8u2e8";
+		}
+	}
+
+	@Ignore
+	public void ignoredMethod() {
+	}
+
 
 }
--- trunk/curriculum/lib/src/edu/berkeley/eduride/testing/tests/SquareTest.java
+++ trunk/curriculum/lib/src/edu/berkeley/eduride/testing/tests/SquareTest.java
@@ -2,19 +2,28 @@ package edu.berkeley.eduride.testing.tests;
 import static org.junit.Assert.*;
 
 import java.lang.annotation.*;
+
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(CustomRunner.class)
-public class SquareTest extends EduRideJunitTestClass{
+public class SquareTest extends edu.berkeley.eduride.EduRideTest {
 	// Nate: I didn't delete the annotations. I just put them in 
 	// SquareTestComplicated for now, cuz I wanted to test with a simple test case first
+	
+	@Ignore
+	@Test
+	public void ignoreThis() {
+		fail("THIS IS THE FAIL STRING yo");
+	}
 	
 	@Test
 	public void testAssertSame() {
 		assertSame("assertSame failed", 1,1);
 	}
 
+	@Ignore
 	@Test
 	public void testAssertArrayEquals() {
 		int[] x = {1,2,3,4,5,6,7,8,8,0};
@@ -26,21 +35,16 @@ public class SquareTest extends EduRideJunitTestClass{
 	public void testAssertTrue() {
 		int obs = new Square(5).area();
 		assertTrue("assertTrue failed", 25 == obs);
+
 	}
 	
 	@Test
-	public void testArea1() {
+	public void testArea() {
 		int obs = new Square(5).area();
 		assertEquals("testArea1 failed", 25, obs);
 	}
 	
-	@Test
-	public void testArea2() {
-		int exp = 9;
-		int obs = new Square(3).area();
-		assertEquals("testArea2 failed", exp, obs);
-	}
-	
+
 	
 	@Test
 	public void testPerimeter() {
