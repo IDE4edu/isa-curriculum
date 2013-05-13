59399b2d1d3b8dc2166c38993f8d5257c1c1e562
Nathaniel Titterton <nate@berkeley.edu>
2013-05-13 16:17:49 -0700

    better testing through eduride

--- trunk/curriculum/recursion/reverse-helpers/src/reverseHelpers/ReverseTest.java
+++ trunk/curriculum/recursion/reverse-helpers/src/reverseHelpers/ReverseTest.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.*;
 import java.util.Stack;
 import org.junit.Test;
 
-public class ReverseTest {
+public class ReverseTest extends edu.berkeley.eduride.EduRideTest {
 
 	// keeps track of reverse calls, to test whether reverse calls itself
 	class ReverseChild extends Reverse {
@@ -43,21 +43,24 @@ public class ReverseTest {
 
 
 	@Test
+	@Name("reverse(\"abcde\")")
 	public void correctly_reversed_abcde() {
 		String s = "abcde";
 		Reverse r = new Reverse();
-		assertEquals("failed with call reverse(\""+s+"\") ", real_reverse(s), r.reverse(s));
+		assertEquals("reverse(\""+s+"\") failed", real_reverse(s), r.reverse(s));
 	}
 
 
 	@Test
+	@Name("Testing a long string")
 	public void correctly_reverses_public_static_void_pain() {
 		String s = "public static void pain";
 		Reverse r = new Reverse();
-		assertEquals("failed with call reverse(\""+s+"\") ", real_reverse(s), r.reverse(s));
+		assertEquals("reverse(\""+s+"\") failed", real_reverse(s), r.reverse(s));
 	}
 
 	@Test
+	@Name("reverse(\"Z\")")
 	public void correctly_reverses_Z() {
 		String s = "Z";
 		Reverse r = new Reverse();
@@ -65,22 +68,24 @@ public class ReverseTest {
 	}
 
 	@Test
+	@Name("Mystery string")
 	public void correctly_reverses_not_revealed() {
 		Reverse r = new Reverse();
 		String s = "Computing science is no more about computers than astronomy is about telescopes -- Edsger W. Dijkstra";
 		String out = r.reverse(s);
 		assertTrue(
-				"failed with call we won't reveal! ",
+				"failed with mystery string!",
 				out.equals(real_reverse(s)));
 	}
 
 	@Test
+	@Name("reverse(\"\")")
 	public void correct_when_empty() {
 		Reverse r = new Reverse();
 		String empty = "";
 		try {
 			String out = r.reverse(empty);
-			assertEquals("failed with call reverse(\"\") [the empty string] ",
+			assertEquals("reverse(\"\") [the empty string] failed",
 					"", out);
 		} catch (Exception e) {
 			fail("Your reverse method broke when passed an empty string!");
@@ -89,6 +94,7 @@ public class ReverseTest {
 	}
 
 	@Test
+	@Name("Uses recursion")
 	public void uses_recursion() {
 		String s = "Computing science is no more about computers than astronomy is about telescopes -- Edsger W. Dijkstra";
 		ReverseChild rc = new ReverseChild();
