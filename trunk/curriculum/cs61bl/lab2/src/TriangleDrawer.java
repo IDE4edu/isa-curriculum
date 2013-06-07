98e95c71394eeb29b85e4392cf9e1c306686e30e
Nathaniel Titterton <nate@berkeley.edu>
2013-06-07 16:08:26 -0700

    added TriangleDrawer

---
+++ trunk/curriculum/cs61bl/lab2/src/TriangleDrawer.java
@@ -0,0 +1,22 @@
+
+public class TriangleDrawer {
+
+	public static void main (String[] args) {
+		
+		//These lines need to be rearranged; add right braces as necessary
+		col = col + 1;
+		int col = 0;
+		int row = 0;
+		int SIZE = 10;
+		row = row + 1;
+		System.out.print ('*');
+		System.out.println ('*');
+		System.out.println ( );
+		while (col <= row) {
+		while (col < row) {
+		while (row < SIZE) {
+		while (row <= SIZE) {
+	
+	}
+	
+}
