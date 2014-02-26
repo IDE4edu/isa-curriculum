3f7d270da59e33fb307b5b8ec2da2184802fac72
haladiab-ucb <haladiab1988@berkeley.edu>
2014-02-26 14:04:13 -0800

    isEvenOdd
    Fixed the setting of isEvenOdd

---
+++ trunk/curriculum/isEvenOdd/src/number/NumberCheck.java
@@ -0,0 +1,33 @@
+/**
+ * Fill in the two methods for the NumberCheck class below using what you know
+ * about booleans and conditionals. isEven() should return true if the variable
+ * num is even, and false otherwise. isOdd() should return true if the variable
+ * num is odd, and false otherwise.
+ */
+package number;
+
+public class NumberCheck {
+
+	int myNum;
+
+	// Constructor
+	public NumberCheck(int n) {
+		myNum = n;
+	}
+
+	public boolean isEven() {
+		//Fill in the )testing predicate and conditions below so that isEven works correctly
+		if (myNum%2==0) {                                            
+			return false;  
+		} else {
+			return false;
+		}
+
+   }
+
+	public boolean isOdd() {
+		//fill in the method body so isOdd works correctly
+		return false;
+	}
+}
+
--- trunk/curriculum/isEvenOdd/src/test/Repeat.java
+++ trunk/curriculum/isEvenOdd/src/test/Repeat.java
@@ -1,4 +1,4 @@
-package src.test;
+package test;
 
 import java.lang.annotation.*;
 
